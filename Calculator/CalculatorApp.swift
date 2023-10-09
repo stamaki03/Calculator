9759ae592c51ffff6664a10493b1f076ae872524
stamaki03 <stamaki03@gmail.com>
2023-10-09 21:53:00 +0900

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Sho Tamaki on 2023/10/09.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
